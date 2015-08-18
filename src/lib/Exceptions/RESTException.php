6ab8ac90aae52026f9b8eb7a54a96237b37fbefe
Robert Main <lenswipe@gmail.com>
2015-08-19 00:31:40 +0100

    Added documentation and setup namespacing

--- src/lib/Exceptions/RESTException.php
+++ src/lib/Exceptions/RESTException.php
@@ -1,14 +1,29 @@
 <?php
-
 /**
- * Author: Robert Main
- * Date: 14/08/15
- * Time: 21:58
+ * REST Exception
+ *
+ * General purpose REST exception
+ * 
+ * 
+ * @author Robert Main
+ * @package Communique\lib\Exceptions
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  */
 
 namespace Communique\lib\Exceptions;
 
+/**
+ *
+ * Exception
+ *
+ * This exception is a general purpose exception used to handle all nondescript errors with REST calls.
+ * 
+ */
 class RESTException extends Exception{
+	/**
+	 * Constructor for general purpose REST Exception
+	 * @param String $message A human readable description of the exception
+	 */
 	public function __construct($message){
 		parent::__construct($message);
 	}
--- src/lib/Exceptions/RestConnectionException.php
+++ src/lib/Exceptions/RestConnectionException.php
@@ -1,14 +1,30 @@
 <?php
 
 /**
- * Author: Robert Main
- * Date: 14/08/15
- * Time: 21:58
+ * REST Connection Exception
+ *
+ * REST Exception to be used for all connection related issues.
+ * 
+ * @author Robert Main
+ * @package Communique\lib\Exceptions
+ * @category Exception
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  */
 
 namespace Communique\lib\Exceptions;
 
+/**
+ *
+ * Exception
+ *
+ * This exception is thrown for connection errors when attempting to reach the API.
+ * 
+ */
 class RESTConnectionException extends RESTException{
+	/**
+	 * Constructor for REST Connection Exception
+	 * @param String $message A human readable description of the exception
+	 */
 	public function __construct($message){
 		parent::__construct($message);
 	}
