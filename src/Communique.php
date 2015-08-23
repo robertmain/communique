ab29e46b3b22515584c057c7a32ba54e80da15db
Robert Main <lenswipe@gmail.com>
2015-08-23 03:03:32 +0100

    Provided usage example in documentation

--- src/Communique.php
+++ src/Communique.php
@@ -6,14 +6,75 @@
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
+ * 
+ * 
  */
 
 namespace Communique;
 
 /**
- * Communique Client Library
+ * Communique Pluggable REST Client
  *
- * HTTP Client used for making HTTP requests. This can be swapped out for another if desired.
+ * 
+ * ###Typical Usage
+ * 
+ * You can make a request with the REST library using the code below. It is worth noting, the get, put, post, delete etc. methods 
+ * do not return the raw response payload, but an encapsulation object of type \Communique\RESTClientResponse. This object contains
+ * 1. The HTTP status code (200, 201, 404, 500 etc.)
+ * 1. The response payload (the response body from the server)
+ * 1. The server headers (any headers that the server returned with the payload, these are often useful for cache control).
+ * 
+ * You can find more information from the [\Communique\RESTClientResponse](Communique.RESTClientResponse.html) documentation
+ * 
+ * ```php
+ * <?php
+ *      $rest = new \Communique\Communique('http://api.company.com/');
+ *      $response = $rest->get('users/1'); //Contains information about user number 1
+ *      // Since $response is actually a RESTClientResponse object (rather than the raw response payload), we can get
+ *      // properties of the request like so:
+ * 
+ *      echo $response->status; //This will be the HTTP status code
+ *      // If we want the raw request payload we do this:
+ *      echo $response->payload;
+ *      // Headers can be retrieved like so:
+ *      echo $response->getHeader('header_key');
+ * ?>
+ * ```
+ *
+ * ###Request Interceptors
+ *
+ * Whilst the above example is useful for making simple requests and returning the result from the API,
+ * you may wish to have a little more control over the request. Communique provides a method to do this using Interceptors. 
+ * 
+ * An interceptor is a class with request and response methods. The request method of each interceptor is called on each request
+ * and each the response interceptor method is called on each response. This allows the complex modification of requests juts before
+ * they are sent and just after they are retured. This allows for things like JSON parsing, OAuth request signing or caching.
+ * Interceptors are executed in the order in which they are provided.
+ * 
+ * If you wish to add an interceptor, you may do so by passing an array with an instance of your interceptor as the second constructor
+ * argumment to Communique.
+ *
+ * **Interceptors should implement the [\Communique\Interceptor](Communique.Interceptor.html) interface**
+ * 
+ * ```php
+ * <?php
+ *      $rest = new \Communique\Communiqe('http://api.company.com/', array(new JSONParser(), new OAuth()));
+ *      // Use the library as before
+ * ?>
+ * ```
+ * 
+ * ##Custom HTTP Client
+ * 
+ * This library ships out of the box with a cURL implementation, however if you wish to provide your own you may do so
+ * using the third constructor argument as follows:
+ * 
+ * ```php
+ * <?php
+ *     $rest = new \Communique\Communiqe('http://api.company.com/', array(new JSONParser(), new OAuth()), new CustomHTTPClient());
+ *     // Use the library as before
+ * ?>
+ * ```
+ * 
  * 
  * @author Robert Main
  * @package Communique
@@ -60,8 +121,8 @@ class Communique{
 
 	/**
 	 * Makes the HTTP request using the HTTP client passed into the constructor(defaults to cURL).
-	 * @param  RESTClientRequest $request A RESTClientRequest object encapsulating the request
-	 * @return RESTClientResponse A RESTClientResponse object encapsulating the response
+	 * @param  \Communique\RESTClientRequest $request A RESTClientRequest object encapsulating the request
+	 * @return \Communique\RESTClientResponse A RESTClientResponse object encapsulating the response
 	 * @todo bubble the request and response through the interceptors
 	 */
 	private function _call(\Communique\RESTClientRequest $request){
