3500fec0acf068449098a9fba276b18e83bcfdf7
Robert Main <lenswipe@gmail.com>
2015-08-24 20:59:06 +0100

    Updated composer.lock

--- test/CommuniqueTest.php
+++ test/CommuniqueTest.php
@@ -6,6 +6,12 @@ class CommuniqueTest extends PHPUnit_Framework_TestCase{
     	$this->http = $this->getMockBuilder('\Communique\HTTPClient')->setMethods(array('request'))->getMock();
     }
 
+	/**
+	 * - Loosen up requirement for equalTo
+	 * - Check HTTP verb
+	 * - Check URL
+	 * - Check it's an instance/implementation of HTTPClient
+	 */
     public function testGet(){
     	$rest = new \Communique\Communique('http://domain.com/', array(), $this->http);
     	$this->http->expects($this->once())->method('request')->with($this->equalTo(new \Communique\RESTClientRequest('get', 'http://domain.com/users', 'request+payload', array())));
