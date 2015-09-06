83eba4e0695990a8abef29e22fb3f35abf11e1f3
Robert Main <lenswipe@gmail.com>
2015-09-06 20:16:40 +0100

    Testing for HTTP PUT ref #4

--- test/CurlHTTPClientTest.php
+++ test/CurlHTTPClientTest.php
@@ -57,4 +57,44 @@ class CurlHTTPClientTest extends PHPUnit_Framework_TestCase{
         PHPUnit_Framework_TestCase::assertTrue(array_key_exists(CURLOPT_HTTPHEADER, $curlopts));
         PHPUnit_Framework_TestCase::assertEquals($curlopts[CURLOPT_HTTPHEADER], array('Example: header'));
     }
+
+    public function test_put(){
+        $curlopts = array();
+        $this->curl->method('setopt')
+                    ->will($this->returnCallback(function($curlopt, $value) use (&$curlopts){
+                        $curlopts[$curlopt] = $value;
+                    }));
+
+        $this->curl->method('setopt_array')
+                    ->will($this->returnCallback(function($optarray) use (&$curlopts){
+                        foreach($optarray as $optkey => $optvalue){
+                            $curlopts[$optkey] = $optvalue;
+                        }
+                    }));
+
+        $this->curl->expects($this->once())
+                    ->method('exec')
+                    ->will($this->returnCallback(function(){
+                        return array(
+                            'http_status_code' => 200,
+                            'body' => 'response+payload',
+                            'headers' => array()
+                        );
+                    }));
+
+        $request = new \Communique\RESTClientRequest('PUT', 'https://domain.com/users', array('example' => 'payload'), array('Example' => 'header'));
+        $this->http_client->request($request);
+        
+        PHPUnit_Framework_TestCase::assertTrue(array_key_exists(CURLOPT_PUT, $curlopts));
+        PHPUnit_Framework_TestCase::assertEquals($curlopts[CURLOPT_PUT], true);
+
+        PHPUnit_Framework_TestCase::assertTrue(array_key_exists(CURLOPT_URL, $curlopts));
+        PHPUnit_Framework_TestCase::assertEquals($curlopts[CURLOPT_URL], 'https://domain.com/users');
+
+        PHPUnit_Framework_TestCase::assertTrue(array_key_exists(CURLOPT_POSTFIELDS, $curlopts));
+        PHPUnit_Framework_TestCase::assertEquals($curlopts[CURLOPT_POSTFIELDS], array('example' => 'payload'));
+
+        PHPUnit_Framework_TestCase::assertTrue(array_key_exists(CURLOPT_HTTPHEADER, $curlopts));
+        PHPUnit_Framework_TestCase::assertEquals($curlopts[CURLOPT_HTTPHEADER], array('Example: header'));
+    }
 }
