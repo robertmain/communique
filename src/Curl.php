e3b38637a493a608fe31fd87c50f4a42d416f460
Robert Main <lenswipe@gmail.com>
2015-09-08 21:35:19 +0100

    Resolves #23

--- src/Curl.php
+++ src/Curl.php
@@ -37,7 +37,7 @@ class Curl{
 		if(!extension_loaded('curl')){
 			throw new \Communique\CommuniqueRESTConnectionException('cURL Error: ' . $this->error() . ' cURL Error Code: ' . $this->errno());
 		} else {
-			$this->_ch = curl_init();
+			$this->_ch = curl_init($url);
 		}
 	}
 
